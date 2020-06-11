482065f37b67955650ff9d7541c96f6104527235
nurbek82 <nurbek82@gmail.com>
2020-06-11 15:17:40 -0700

    sending response with html
    we set the "Content-Type" key to "text/html; charset=UTF-8;
    we creat html variable to hld the html content that we will write to the body of the response. We use back tick (`)
    we send the html with the response using response.end(html)
    we log the requested URL.

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-//require http module
 const http = require('http');
-
-//create http server 
-const server = http.createServer(function(request,response) {
-    
+const server = http.createServer((request, response) => {
     const head = {
-        'Content-Type':'title/plain'
-    }  
-//send the response header to the request
-response.writeHead(200,head)
+        'Content-Type': 'text/html; charset=UTF-8'
+    }
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Hello, world!</title>
+    </head>
+    <body>
+       <h1>Hello, world!</h1>
+    </body>
+    </html>`;
 
-//send the response body  and indicate that message is complete
-  
-  response.end('Hello World';)
-})
- 
-//start http server listening for connections
-server.listen(8080)
- 
+    response.writeHead(200, head);
+    response.end(html);
+    console.log(request.url);
+});
 
- 
-
+server.listen(8080);
  
